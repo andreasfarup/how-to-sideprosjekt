70bc787a5c1e87205a2f0c98f467572f5c79e474
Andreas Aasheim Farup <andreasfarup@gmail.com>
2024-03-20 17:17:59 +0100

    Test 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-center">
-      <h1 className="text-2xl">Hei, jeg heter Andreas!</h1>
+      <h1 className="text-2xl">Hei, jeg heter Andreas Aasheim Farup!</h1>
       <p>Jeg studerer datateknologi på Universitetet i Bergen.</p>
     </div>
   );
