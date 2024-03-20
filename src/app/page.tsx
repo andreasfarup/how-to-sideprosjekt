ef7ba54292bf13dc681cde0f60805ca572731da0
Andreas Aasheim Farup <andreasfarup@gmail.com>
2024-03-20 17:31:25 +0100

    Mikal list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 export default function Home() {
   return (
-    <div className="text-center">
-      <h1 className="text-2xl">Hei, jeg heter Andreas Aasheim Farup!</h1>
-      <p>Jeg studerer datateknologi på Universitetet i Bergen.</p>
+    <div className="min-h-screen bg-blue-900 text-white px-4">
+      <div className="max-w-screen-sm mx-auto text-lg flex flex-col gap-6 py-24">
+        <h1 className="text-4xl font-bold text-white">
+          👋🏻 Hei, jeg heter Andreas Aasheim Farup!
+        </h1>
+        <p>
+          Jeg studerer Datateknologi på Universitetet i Bergen 💻🤓
+        </p>
+
+        <p>🚀 Ting jeg er bedre enn i Mikal i:</p>
+
+        <ul className="pl-4 list-disc">
+          <li>Bordtennis 🏓</li>
+          <li>Soccer Heads ⚽️</li>
+        </ul>
+      </div>
     </div>
   );
 }
