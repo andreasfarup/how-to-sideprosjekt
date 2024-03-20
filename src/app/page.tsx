cdc9d7b4aa27d4eb1824502a62db68f5dc94cfd4
Andreas Aasheim Farup <andreasfarup@gmail.com>
2024-03-20 17:15:53 +0100

    First test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center h-full gap-10 p-24">
-      <h1 className="text-3xl font-medium">
-        Velkommen til {'"'}How to sideprosjekt{'"'}
-      </h1>
-
-      <p>
-        Om du vil ha oppgaver, les{" "}
-        <span className="bg-gray-200 font-mono py-1 px-2 rounded-md">
-          README.md
-        </span>
-        .
-      </p>
-    </main>
+    <div className="text-center">
+      <h1 className="text-2xl">Hei, jeg heter Andreas Aasheim Farup!</h1>
+      <p>Jeg studerer datateknologi på Universitetet i Bergen.</p>
+    </div>
   );
 }
